e9af298453e04e64c438b71b316aa22f6c95ef5f
mkondratek <mkondratek@virtuslab.com>
2021-01-06 22:25:10 +0100

    Add created_by field; Migrate

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
@@ -5,6 +6,7 @@ from django.urls import reverse
 class Note(models.Model):
     title = models.CharField(max_length=256)
     body = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse('note-detail', kwargs={'pk': self.pk})
