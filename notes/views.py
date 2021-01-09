fff1406a14c9306164956bb9cb4faa9550510881
mkondratek <mkondratek@virtuslab.com>
2021-01-09 11:42:56 +0100

    Require login to create notes

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -6,9 +7,13 @@ from django.views.generic.list import ListView
 from notes.models import Note
 
 
-class NoteCreate(CreateView):
+class NoteCreate(LoginRequiredMixin, CreateView):
     model = Note
     fields = ['title', 'body']
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super(NoteCreate, self).form_valid(form)
 
 
 class NoteUpdate(UpdateView):
