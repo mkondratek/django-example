8b9567c649bb9f97ce5be10f681fce6ecbe66c99
mkondratek <mkondratek@virtuslab.com>
2021-01-09 12:02:26 +0100

    Make note TimeStampedModel

--- notes/models.py
+++ notes/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django_extensions.db.models import TitleSlugDescriptionModel, TimeStampedModel
 
 
-class Note(models.Model):
+class Note(TimeStampedModel):
     title = models.CharField(max_length=256)
     body = models.TextField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
